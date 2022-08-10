1e6790acc60c838d61a0d46a180fdab79c95019e
Tosif Attar <tosattar@publicisgroupe.net>
2022-08-11 01:07:53 +0530

    mongodb changes added

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,7 +13,14 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, price, description, imageUrl);
+  const product = new Product(
+    title,
+    price,
+    description,
+    imageUrl,
+    null,
+    req.user._id
+  );
   product
     .save()
     .then((result) => {
@@ -33,15 +40,16 @@ exports.getEditProduct = (req, res, next) => {
   }
   const prodId = req.params.productId;
   Product.findById(prodId)
-    .then((products) => {
-      if (!products) {
+    // Product.findById(prodId)
+    .then((product) => {
+      if (!product) {
         return res.redirect("/");
       }
       res.render("admin/edit-product", {
         pageTitle: "Edit Product",
         path: "/admin/edit-product",
         editing: editMode,
-        product: products,
+        product: product,
       });
     })
     .catch((err) => console.log(err));
@@ -61,7 +69,6 @@ exports.postEditProduct = (req, res, next) => {
     updatedImageUrl,
     prodId
   );
-
   product
     .save()
     .then((result) => {
@@ -80,9 +87,7 @@ exports.getProducts = (req, res, next) => {
         path: "/admin/products",
       });
     })
-    .catch((err) => {
-      console.log(err);
-    });
+    .catch((err) => console.log(err));
 };
 
 exports.postDeleteProduct = (req, res, next) => {
--- models/product.js
+++ models/product.js
@@ -2,18 +2,20 @@ const mongodb = require("mongodb");
 const getDb = require("../util/database").getDb;
 
 class Product {
-  constructor(title, price, description, imageUrl, id) {
+  constructor(title, price, description, imageUrl, id, userId) {
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
     this._id = id ? new mongodb.ObjectId(id) : null;
+    this.userId = userId;
   }
 
   save() {
     const db = getDb();
     let dbOp;
     if (this._id) {
+      // Update the product
       dbOp = db
         .collection("products")
         .updateOne({ _id: this._id }, { $set: this });
@@ -22,7 +24,6 @@ class Product {
     }
     return dbOp
       .then((result) => {
-        console.log(result);
       })
       .catch((err) => {
         console.log(err);
@@ -56,12 +57,18 @@ class Product {
         console.log(err);
       });
   }
+
   static deleteById(prodId) {
     const db = getDb();
     return db
       .collection("products")
       .deleteOne({ _id: new mongodb.ObjectId(prodId) })
-      .then((result) => console.log(result));
+      .then((result) => {
+        console.log("Deleted");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 }
 
